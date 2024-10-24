5e61e466de2e5a8ea7a2be4bc15c2f90ad169892
MarioParrilla <marioparrilla2@gmail.com>
2024-10-24 17:01:50 +0200

    init

--- install.sh
+++ install.sh
@@ -9,16 +9,22 @@ function runLinuxInstall() {
     nvimInstalled=$(which nvim | wc -l)
     if [ $nvimInstalled -eq 0 ]
     then
-        apt-get install neovim
+        apt-get install neovim > /dev/null
         if [ $? -ne 0 ]
         then
             echo "[!]: Error installing Neovim!."
             exit 1
         fi
+        echo "[i]: Neovim Installed"
     else
         echo "[i]: Neovim is already installed"
     fi
 }
+
+if [ "$EUID" -ne 0 ]; then
+    echo "[!]: You need to use with root permissions"
+    exit 1
+fi
 
 if [ "$SO" == "Linux" ]; then
     runLinuxInstall
